3fa9a2d82ee9b138df7750feacaa4a3ba024d823
Basirul Billah <basirulbillah@gmail.com>
2022-02-17 06:55:07 +0600

    added updates field function

--- js/app.js
+++ js/app.js
@@ -6,24 +6,26 @@ function getInput(keyword) {
     return inputAmount;
 }
 
+// updates fields
+function updateValues(fieldVariable, updateId) {
+    const outputField = document.getElementById(updateId);
+    outputField.innerText = fieldVariable;
+}
+
 // calculates total expence and shows to user 
 document.getElementById('calculate-button').addEventListener('click', function () {
 
+    // gets input from the input fields
     const incomeAmount = getInput('income');
-
     const foodAmount = getInput('food');
     const rentAmount = getInput('rent');
     const clotheAmount = getInput('clothe');
 
-    // calculates expence 
+    // calculates and updates expence 
     const totalExpence = foodAmount + rentAmount + clotheAmount;
+    updateValues(totalExpence, 'total-expence');
 
-    const totalExpenceOutput = document.getElementById('total-expence');
-    totalExpenceOutput.innerText = totalExpence;
-
-    // calculates balance 
+    // calculates and updates balance 
     const balance = incomeAmount - totalExpence;
-
-    const balanceOutput = document.getElementById('balance-output');
-    balanceOutput.innerText = balance;
+    updateValues(balance, 'balance-output');
 })
