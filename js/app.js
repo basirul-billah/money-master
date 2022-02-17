d54c7f8112ae281dd0463bc18ad341919dcdcd56
Basirul Billah <basirulbillah@gmail.com>
2022-02-17 09:16:38 +0600

    added saving and remainning calculation

--- js/app.js
+++ js/app.js
@@ -29,3 +29,24 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const balance = incomeAmount - totalExpence;
     updateValues(balance, 'balance-output');
 })
+
+// calculating and displaying saving amount and remainning amount 
+document.getElementById('save-button').addEventListener('click', function () {
+    // gets input from the savings input field
+    const savingsPercentage = getInput('savings');
+
+    // gets current balance from html text 
+    const currentBalance = document.getElementById('balance-output').innerText;
+    
+    // calculating savings 
+    const savingsAmount = parseFloat(currentBalance) * (savingsPercentage / 100);
+    
+    // calulating remaining amount 
+    const remainingAmount = currentBalance - savingsAmount;
+
+    // displaying savings amount 
+    document.getElementById('saving-amount').innerText = savingsAmount;
+
+    // displaying remainning amount 
+    document.getElementById('remainning-balance').innerText = remainingAmount;
+})
