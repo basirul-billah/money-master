c5436c666356388effd786e3303f42d0d6bb0961
Basirul Billah <basirulbillah@gmail.com>
2022-02-17 06:19:58 +0600

    added js file

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// calculates total expence and shows to user 
+function totalExpence() {
+    const incomeInput = document.getElementById('income-input');
+    const incomeAmount = parseFloat(incomeInput.value);
+
+    const 
+
+    return console.log(incomeAmount);
+}
